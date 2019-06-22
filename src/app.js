37e83d8a84ca9c94f641ab331cb1cad63b093f73
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-22 09:15:56 -0400

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Defined paths for express config
 const publicPath = path.join(__dirname, '../public');
@@ -96,6 +97,8 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server up on port 3000')
+
+
+app.listen(port, () => {
+    console.log(`server up on port 3000 ${port}`)
 })
