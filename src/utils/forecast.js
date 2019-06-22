9e4fbfb698121352ece0cad00a583146312282da
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-22 19:32:22 -0400

    add daily high nad daily low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,14 +4,14 @@ const forecast = (latitude, longitude, cb) => {
     const url = `https://api.darksky.net/forecast/cb0c401500f2621c9cd3453ceff364e3/${latitude},${longitude}`
     request({url, json:true }, (error, response) => {
         const { temperature, precipProbability} = response.body.currently
-        const { summary } = response.body.daily.data[0]
+        const { summary,temperatureLow, temperatureHigh } = response.body.daily.data[0]
         if (error) {
             cb('no internet', undefined)
         } else if ( response.body.error ) {
             cb('no address found', undefined)
         } else {
             cb( undefined, 
-                `${summary} It is currently ${temperature} degrees. There is a ${precipProbability}% chance of rain.`
+                `${summary} It is currently ${temperature} degrees.\n There is a ${precipProbability}% chance of rain. \n Daily high ${temperatureHigh}.\n Daily Low ${temperatureLow}.`
             )
         }
     })
